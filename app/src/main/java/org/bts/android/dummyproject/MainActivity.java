5654525fa968ca503d6800cf0551247950c61bd9
pedrodelmonte <pdelmonte@gmail.com>
2017-01-30 13:10:19 +0100

    Functionality on the buttons and callback methods for Instance changes,

--- app/src/main/java/org/bts/android/dummyproject/MainActivity.java
+++ app/src/main/java/org/bts/android/dummyproject/MainActivity.java
@@ -3,7 +3,6 @@ package org.bts.android.dummyproject;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -95,18 +94,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
 
         if (whichView.getId() == R.id.btn_main_activity_start_activity) {
 
-            Log.e("OnClickClass", "Button 1 Clicked");
+            Log.e(MainActivity.TAG, "Button 1 Clicked");
 
         } else if (whichView.getId() == R.id.second_btn){
 
-            Log.w("OnClickClass", "Button 2 Clicked");
+            Log.w(MainActivity.TAG, "Button 2 Clicked");
             Intent activityIntent = new Intent(this, SecondActivity.class);
 //            startActivity(activityIntent);
             startActivityForResult(activityIntent, 0);
 
         } else {
 
-            Log.w("OnClickClass", "Button 3 Clicked");
+            Log.w(MainActivity.TAG, "Button 3 Clicked");
 
         }
 
